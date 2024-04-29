e7849bab5f40fd3668a8b5d8502caba522ba361e
mickykwon <168437322+mickykwon@users.noreply.github.com>
2024-04-29 21:28:44 +0900

    first commit

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+  <small>Copyright B-Boy Brothers. All Right reserved.</small>
+</footer>
+
+<style lang='scss'>
+  footer {
+    text-align: center;
+    margin-top: 50px;
+    color: #444;
+  }
+
+  @media (max-width: 516px) {
+    footer {
+      margin-top: 0px;
+      padding: 20px;
+    }
+  }
+</style>
---
+++ src/lib/components/Home.svelte
@@ -0,0 +1,46 @@
+<script>
+  import Header from './Header.svelte';
+  import Footer from './Footer.svelte';
+
+</script>
+
+<Header />
+<main class='container'>
+  <video 
+    autoplay loop muted
+    src="video/title.mp4"
+  ></video>
+</main>
+<Footer />
+
+<style>
+  video {
+    width: 100%;
+    aspect-ratio: 2.35/1;
+    object-fit: cover;
+    object-position: center;
+    display: block;
+  } 
+
+ .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* height: 100vh; */
+    background: #fff;
+  }
+
+  @media (max-width: 516px) {
+    main {
+      height: calc(100vh - 138px - 66px);
+    }
+    video {
+      width: 100%;
+      height: 100%;
+      /* aspect-ratio: 2.35/1; */
+      object-fit: cover;
+      object-position: center;
+      display: block;
+    } 
+  }
+</style>
